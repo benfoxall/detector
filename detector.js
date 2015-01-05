80183bd7e1f2adc25ea13eb9bb963c6087956ca6
Ben Foxall <ben@whiteoctober.co.uk>
2015-01-05 20:30:45 +0000

    stubbed out stuff

---
+++ detector.js
@@ -0,0 +1,33 @@
+function Detector(videoElement){
+  this.videoElement = videoElement;
+
+  // create a webgl context with texture bound to video
+  
+}
+
+// start detecting
+Detector.prototype.start = function(){
+  
+}
+
+// finish detecting
+Detector.prototype.end = function(){
+  
+  return //ImageData
+}
+
+Detector.util = {
+
+  // detect regions in image
+  process: function(imageData){
+    return []
+  },
+
+  // merge a set of point datas
+  merge: function(points1, points2){
+    return []
+  },
+
+  // 
+
+}
